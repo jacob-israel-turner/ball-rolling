eb41cb81fe2852ad7e3f4718ea7b96c9dbff45d6
Jacob Turner <jacob.turner@domo.com>
2017-05-29 18:38:36 -0600

    Pickups and scoring done

--- Assets/Scripts/PlayerControl.js
+++ Assets/Scripts/PlayerControl.js
@@ -1,10 +1,15 @@
 ﻿#pragma strict
+import UnityEngine.UI;
 
 var rb : Rigidbody;
 var speed : int;
+var count : int;
+var countText : Text;
 
 function Start () {
 	rb = GetComponent.<Rigidbody>();
+	count = 0;
+	updateCount();
 }
 
 function FixedUpdate () {
@@ -12,4 +17,16 @@ function FixedUpdate () {
 	var moveVertical : float = Input.GetAxis('Vertical');
 	var movement : Vector3 = new Vector3(moveHorizontal, 0, moveVertical);
 	rb.AddForce(movement * speed);
+}
+
+function OnTriggerEnter (other : Collider) {
+	if(other.gameObject.CompareTag('pick_up')) {
+		other.gameObject.SetActive(false);
+		count++;
+		updateCount();
+	}
+}
+
+function updateCount() {
+	countText.text = "Count: " + count.ToString();
 }
